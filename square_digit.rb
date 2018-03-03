617ee441a39d1e1c38006fd9251ce9194d6fc139
Ashish Rao <ashishrao2598@gmail.com>
2018-03-04 00:40:24 +0530

    Square every digit problem

---
+++ square_digit.rb
@@ -0,0 +1,24 @@
+# Square every digit
+# Problem statement: square every digit of a number
+# For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+# initial solution
+def square_digits num
+    number = num.to_s.split("")
+    number.map! {|i| i.to_i * i.to_i}
+    number.join("").to_i
+  end
+
+# optimized solution
+def square_digits num
+    # code goes here
+    num.to_s.chars.map{|x| x.to_i**2}.join.to_i
+end
+  
+  # test cases
+  
+  square_digits(3212)
+  square_digits(2112)
+  square_digits(1111)
+  square_digits(4545)
+  square_digits(2343)
