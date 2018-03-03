94eb7deba52fb0be3088bb4661f18990fa2b48c7
Ashish Rao <ashishrao2598@gmail.com>
2018-03-04 00:27:59 +0530

    gauss sum problem

---
+++ gauss_sum.rb
@@ -0,0 +1,20 @@
+# Gauß needs help! (Sums of a lot of numbers)
+# problem statement: Due to another of his misbehaved, the primary school's teacher of the young Gauß, Herr J.G. Büttner, to keep the bored and unruly young schoolboy Karl Friedrich Gauss busy for a good long time, while he teaching arithmetic to his mates, assigned him the problem of adding up all the whole numbers from 1 through a given number n. Your task is to help the young Carl Friedrich to solve this problem as quickly as you can; so, he can astonish his teacher and rescue his recreation interval.
+
+# example: f(n=100) -->returns 5050
+
+
+def add(num)
+    if(num.class == Fixnum and num > 0)
+        sum  = (1..num).to_a.inject(:+)
+        return sum
+    else
+        return false
+    end
+end
+
+# test cases
+add(100)
+add(-11)
+add(1.34)
+add(200)
