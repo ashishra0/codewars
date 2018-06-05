36d070eafbcc13721e36ba7d52159085b5a504d7
Ashish Rao <ashishrao2598@gmail.com>
2018-06-05 15:47:41 +0530

    Refactored Code

--- vending_machine.rb
+++ vending_machine.rb
@@ -8,30 +8,42 @@ class VendingMachine
      {:name=>"Dairy Milk", :code=>"A03", :quantity=>1, :price=>0.65},
      {:name=>"Freddo", :code=>"A04", :quantity=>1, :price=>0.25}]
     @machine_money = machine_money
-    # binding.pry
+  end
+
+  def selected_item(code)
+    @items.detect { |item| item[:code]==code}
+  end
+
+  def item_stock(code)
+    selected_item(code)[:quantity]
+  end
+
+  def change(code, money)
+    money - selected_item(code)[:price]
+  end
+
+  def item_name(code)
+    selected_item(code)[:name]
+  end
+
+  def new_stock(code)
+    selected_item(code)[:quantity]+=1
   end
 
   def vend(code, money)
     if @items.any?{|item| item.has_value?(code)}
-      user_selected_item = @items.detect { |item| item[:code]==code}
-      item_stock = user_selected_item[:quantity]
-      change = money - user_selected_item[:price]
-      if money < user_selected_item[:price] and item_stock > 0
+      if money < selected_item(code)[:price] and item_stock(code)>0
         puts "Not enough money!"
       else
-        puts "vending #{user_selected_item[:name]} with #{change} change." and item_stock
+        puts "vending #{item_name(code)} with #{change(code, money)} change." and item_stock(code)
       end
-      puts "#{user_selected_item[:name]}: Out of stock!" if item_stock==0
-      # keeping track of stock and machine_money
-      user_selected_item[:quantity] += 1
-      machine_money = @machine_money
+        puts "#{item_name(code)}: Out of stock!" if item_stock(code)==0
+        new_stock(code)
     else
-      puts "Invalid selection! : Money in vending machine = #{@machine_money}"
+       puts "Invalid selection! : Money in vending machine = #{@machine_money}"
     end
   end
 end
 
 obj = VendingMachine.new(@items, 11.20)
-# puts obj.vend("A01", 10.50)
-# puts obj.vend("A07", 9.50)
-ap obj.vend("z09", 10.00)
+ap obj.vend("A04", 0.10)
