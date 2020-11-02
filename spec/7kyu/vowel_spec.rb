a253feeee61edec15597b4c186921e185c6719de
Ashish Rao <ashishrao2598@gmail.com>
2020-11-02 23:13:34 +0530

    add unit test

---
+++ spec/7kyu/vowel_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../../lib/7kyu/vowel.rb'
+RSpec.describe 'get_count' do
+  it 'returns the count of vowels in the string' do
+    expect(get_count("abracadabra")).to eq(5)
+  end
+
+  it 'returns 0 if string has no vowels' do
+    expect(get_count("hjklgnb")).to eq(0) 
+  end
+end
