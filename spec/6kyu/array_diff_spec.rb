b5a9e899e4296619b420cb31af906e6ef7aa679b
Ashish Rao <ashishrao2598@gmail.com>
2020-11-03 00:36:47 +0530

    add unit test

---
+++ spec/6kyu/array_diff_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../../lib/6kyu/array_diff.rb'
+
+RSpec.describe 'array_diff' do
+  it 'removes identical values from two arrays' do
+    one = ['a', 'z', 'e', 1, 45]
+    two = ['z', 45, 'r', 't']
+    res = ['a', 'e', 1]
+    expect(array_diff(one, two)).to eq(res)
+  end
+end
