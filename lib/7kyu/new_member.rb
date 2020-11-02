535be18ea4907c81e2fd2b227542ecd2bdd4cd2b
Ashish Rao <ashishrao2598@gmail.com>
2020-11-02 23:10:52 +0530

    refactor code

--- lib/7kyu/new_member.rb
+++ lib/7kyu/new_member.rb
@@ -9,12 +9,6 @@
 # OUTPUT
 # Output will consist of a list of string values["Open"] or ["Senior"] stating whether the respective member is to be placed in the senior or open category.
 
-def openOrSenior(data)
+def open_or_senior(data)
     data.map {|age,handicap| age >= 55 && handicap > 7 ? "Senior" : "Open" }
 end
-
-
-openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])
-openOrSenior([[3, 12],[55,1],[91, -2],[54, 23]])
-openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]])
-openOrSenior([[16, 23],[73,1],[56, 20],[1, -1]])
