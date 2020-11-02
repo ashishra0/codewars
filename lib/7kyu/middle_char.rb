ff0f76b957ec5a22df3485af6144c3ed57b60578
Ashish Rao <ashishrao2598@gmail.com>
2020-11-02 23:09:44 +0530

    refactor code

--- lib/7kyu/middle_char.rb
+++ lib/7kyu/middle_char.rb
@@ -2,22 +2,6 @@
 # Problem statment: You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
 def get_middle(s)
-    if
-        s.length.even?
-        new_string = s.slice(s.length/2-1,2)
-        return new_string
-    else
-        s.length.odd?
-        new_string = s.slice(s.length/2)
-        return new_string
-    end
+    s.length.even? ? s.slice(s.length/2-1,2) : s.slice(s.length/2)
 end
-
-# test cases
-
-get_middle("test")
-get_middle("testing")
-get_middle("OkdWuJtDMupi")
-get_middle("TVaBkqeHXOVTbNMe")
-get_middle("NwDCmNDAedmLOxXZTYtz")
 
