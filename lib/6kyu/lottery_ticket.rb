8c34163e3fe4f3c3f1c129518fe5969f46743b99
Ashish Rao <ashishrao2598@gmail.com>
2020-11-03 00:35:47 +0530

    remove rspec code

--- lib/6kyu/lottery_ticket.rb
+++ lib/6kyu/lottery_ticket.rb
@@ -9,21 +9,3 @@ def bingo(ticket, win)
   end
   count >= win ? "Winner!" : "Loser!"
 end
-
-RSpec.describe "bingo" do
-  context "winning scenario" do
-    it "decides lottery winner" do
-      arr = [['ABC', 65], ['HGR', 74], ['BYHT', 74]]
-      win = 1
-      expect(bingo(arr, win)).to eq('Winner!')
-    end
-  end
-
-  context "losing scenario" do
-    it "decides lottery loser" do
-      arr = [['ABC', 65], ['HGR', 74], ['BYHT', 74]]
-      win = 2
-      expect(bingo(arr, win)).to eq('Loser!')
-    end
-  end
-end
