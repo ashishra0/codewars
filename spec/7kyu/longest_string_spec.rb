22ab2687d3b891138690938f58df78eb1c389682
Ashish Rao <ashishrao2598@gmail.com>
2020-11-02 23:09:29 +0530

    add unit test

---
+++ spec/7kyu/longest_string_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../../lib/7kyu/longest_string.rb'
+
+RSpec.describe 'longest' do
+  it 'should return longest unique string' do
+    s1 = "aretheyhere"
+    s2 = "yestheyarehere"
+    output = "aehrsty"
+    expect(longest(s1, s2)).to eq(output)
+  end
+end
