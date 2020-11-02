36a137741c6104bb9efbe13327ab34e3e2574584
Ashish Rao <ashishrao2598@gmail.com>
2020-11-02 23:10:42 +0530

    add unit test

---
+++ spec/7kyu/minimum_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../lib/7kyu/minimum.rb'
+
+RSpec.describe 'remove_smallest' do
+  it 'removes the element with lower index' do
+    arr = [43,32,12,1,2]
+    output = [43,32,12,2]
+    expect(remove_smallest(arr)).to eq(output)
+  end
+
+  it 'returns empty array' do
+    expect(remove_smallest([])).to eq([])
+  end
+end
