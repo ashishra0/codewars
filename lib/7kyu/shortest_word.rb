4cd0d643ac0b82520b8d1f1c6106d395e3a60701
Ashish Rao <ashishrao2598@gmail.com>
2020-11-02 23:12:27 +0530

    refactor code

--- lib/7kyu/shortest_word.rb
+++ lib/7kyu/shortest_word.rb
@@ -3,9 +3,5 @@
 
 def find_short(s)
     l = s.split.sort_by {|a| a.length}
-    return l[0].length
+    l[0].length
 end
-
-# Test cases
-find_short("the world is not enough")
-find_short("Are u going up the hill?")
