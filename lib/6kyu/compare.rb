f85b50ebc5137aeb440ac4b3f13a7f38f35b5ec6
Ashish Rao <ashishrao2598@gmail.com>
2020-11-03 14:41:34 +0530

    refactor code

--- lib/6kyu/compare.rb
+++ lib/6kyu/compare.rb
@@ -1,7 +1,6 @@
 # Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order
 
 def comp(array1, array2)
-  return false unless array1 && array2
-
+  return false if array1.empty? || array2.empty?
   array1.sort.map { |i| i * i } == array2.sort
 end
