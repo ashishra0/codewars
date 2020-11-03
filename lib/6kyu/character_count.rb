ea56273780652816dac9ef4f503fdec839c638cd
Ashish Rao <ashishrao2598@gmail.com>
2020-11-03 14:48:11 +0530

    remove rspec test from source file

--- lib/6kyu/character_count.rb
+++ lib/6kyu/character_count.rb
@@ -9,16 +9,3 @@ def count_chars(s)
   end
   count
 end
-
-RSpec.describe "count_chars" do
-  it 'returns empty hash if string is empty' do
-    str = ""
-    expect(count_chars(str)).to eq({})
-  end
-
-  it 'returns count of each character in a hash' do
-    str = "aba"
-    res = {"a"=>2, "b"=>1}
-    expect(count_chars(str)).to eq(res)
-  end
-end
