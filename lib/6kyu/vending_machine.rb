e1b8b4ec008df20622dfc1fcb60d8798d1697d81
Ashish Rao <ashishrao2598@gmail.com>
2020-11-03 14:43:00 +0530

    refactor code

--- lib/6kyu/vending_machine.rb
+++ lib/6kyu/vending_machine.rb
@@ -1,26 +1,28 @@
-require 'pry-nav'
-require 'awesome_print'
-
 class VendingMachine
   def initialize(items, machine_money)
-    @items = [{:name=>"Smarties", :code=>"A01", :quantity=>10, :price=>0.60},
-     {:name=>"Caramac Bar", :code=>"A02", :quantity=>5, :price=>0.60},
-     {:name=>"Dairy Milk", :code=>"A03", :quantity=>1, :price=>0.65},
-     {:name=>"Freddo", :code=>"A04", :quantity=>1, :price=>0.25}]
+    @items = items
     @machine_money = machine_money
   end
 
   def vend(code, money)
     if @items.any?{|item| item.has_value?(code)}
-      if money < selected_item(code)[:price] and item_stock(code)>0
-        puts "Not enough money!"
+      if money < selected_item(code)[:price] and item_stock(code) > 0
+        "Not enough money!"
+      elsif item_stock(code) == 0
+          "#{item_name(code)}: Out of stock!"
       else
-        puts "vending #{item_name(code)} with #{change(code, money)} change." and item_stock(code)
+        if change(code, money) == 0.0
+          deplete_stock(code)
+          new_stock(code)
+          "vending #{item_name(code)}"
+        else
+          deplete_stock(code)
+          new_stock(code)
+          "vending #{item_name(code)} with #{change(code, money)} change."
+        end
       end
-        puts "#{item_name(code)}: Out of stock!" if item_stock(code)==0
-        new_stock(code)
     else
-       puts "Invalid selection! : Money in vending machine = #{@machine_money}"
+       "Invalid selection! : Money in vending machine = #{@machine_money}"
     end
   end
 
@@ -35,17 +37,20 @@ class VendingMachine
   end
 
   def change(code, money)
-    money - selected_item(code)[:price]
+      money - selected_item(code)[:price]
   end
 
   def item_name(code)
     selected_item(code)[:name]
   end
 
+  def deplete_stock(code)
+    item = @items.find { |i| i[:code] == code }
+    item[:quantity] -= 1
+  end
+
   def new_stock(code)
-    selected_item(code)[:quantity]+=1
+    item = @items.find { |i| i[:code] == code }
+    item[:quantity] += 1
   end
 end
-
-obj = VendingMachine.new(@items, 11.20)
-ap obj.vend("A04", 10.10)
