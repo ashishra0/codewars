d1a086055d33c393618d85a473f4204c9e075d87
Ashish Rao <ashishrao2598@gmail.com>
2020-11-05 00:42:35 +0530

    add rspec test

---
+++ spec/6kyu/highest_number_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../../lib/6kyu/highest_number.rb'
+
+RSpec.describe '#highest_rank' do
+
+  let(:input1) { [12, 10, 8, 12, 7, 6, 4, 10, 12] }
+  let(:input2) { [1, 1, 2, 2, 3] }
+
+  it 'returns the number which is most frequent in the given input array.' do
+    expect(highest_rank(input1)).to eq(12)
+  end
+
+  it 'return the largest number amongst other high occuring numbers' do
+    expect(highest_rank(input2)).to eq(2)
+  end
+end
