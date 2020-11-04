a7c2341834faaed971fc0724d89062636797eda3
Ashish Rao <ashishrao2598@gmail.com>
2020-11-04 20:04:15 +0530

    refactor code

--- lib/7kyu/caeser.rb
+++ lib/7kyu/caeser.rb
@@ -1,20 +1,7 @@
 require 'pry'
 
 def caeser(message, key)
-  codepoints_array = []
-  ciphertext = ""
-
-  a_codepoint = 'a'.ord
-  alphabets = ("a".."z")
-
-  message.split('').each do |letter|
-    if letter =~ /[^a-zA-Z]/
-      codepoints_array << letter.bytes.join('').to_i
-    else
-      shifted_codepoint = letter.downcase.bytes.join('').to_i + key
-      codepoints_array << (shifted_codepoint - a_codepoint) % 26 + a_codepoint
-    end
-  end
-  ciphertext = codepoints_array.pack 'C*'
-  ciphertext.upcase
+  plain = ('A'..'Z').to_a
+  cipher = plain.rotate(key)
+  message.upcase.tr(plain.join, cipher.join)
 end
