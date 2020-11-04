188e5785a0207b8afa3228382540b0b4d0ff6ce7
Ashish Rao <ashishrao2598@gmail.com>
2020-11-05 00:42:28 +0530

    add highest number kata

---
+++ lib/6kyu/highest_number.rb
@@ -0,0 +1,11 @@
+def highest_rank(arr)
+  temp = []
+  result = []
+  arr.each { |n| temp << arr.count(n) }
+  arr.each do |n|
+    if arr.count(n) == temp.max
+      result << n
+    end
+  end
+  result.max.to_s.to_i
+end
