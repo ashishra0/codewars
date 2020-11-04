6c397ee69a012090aaf0a1e47596961d6ff75ba2
Ashish Rao <ashishrao2598@gmail.com>
2020-11-04 13:52:01 +0530

    add rspec test

---
+++ spec/7kyu/caeser_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../../lib/7kyu/caeser.rb'
+
+RSpec.describe '#caeser' do
+
+  let(:message) { "hello" }
+  let(:res1) { "OLSSV" }
+  let(:res2) { "KHOOR" }
+
+  it 'shifts the input string by 7' do
+    expect(caeser(message, 7)).to eq(res1)
+  end
+
+  it 'shifts the input string by 3' do
+    expect(caeser(message, 3)).to eq(res2)
+  end
+end
