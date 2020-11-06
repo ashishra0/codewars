79735020e303e25bcec890a70e25ea791234327b
Ashish Rao <ashishrao2598@gmail.com>
2020-11-06 11:44:26 +0530

    add new 6kyu kata

---
+++ lib/6kyu/smart_traffic_lights.rb
@@ -0,0 +1,19 @@
+class SmartTrafficLight
+  attr_accessor :street1, :street2
+  def initialize(street1, street2)
+    @street1 = street1
+    @street2 = street2
+  end
+
+  def turn_green
+    if street1[0] > street2[0]
+      street1[0] = 0
+      street1[1]
+    elsif street1[0] < street2[0]
+      street2[0] = 0
+      street2[1]
+    elsif street1[0] == street2[0]
+      nil
+    end
+  end
+end
