644fe43dddbc6336780b6b68f8763194d99fc58f
Ashish Rao <ashishrao2598@gmail.com>
2020-11-06 23:08:55 +0530

    fix rubocop issues

--- lib/7kyu/desc_order.rb
+++ lib/7kyu/desc_order.rb
@@ -1,10 +1,10 @@
 # Descending Order
-# problem statement: make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+# problem statement: make a function that can take any non-negative integer as a argument and
+# return it with its digits in descending order.
+# Essentially, rearrange the digits to create the highest possible number.
 
 class Descending
-  def descending_order(n)
-    num = n.to_s.split('').sort.reverse
-    new_number = num.join('').to_i
-    new_number
+  def descending_order(num)
+    num.to_s.split('').sort.reverse.join('').to_i
   end
 end
