45e258a7c809d11404af39876c644604ef840628
Ashish Rao <ashishrao2598@gmail.com>
2021-02-20 21:35:02 +0530

    fix rubocop issues

--- lib/6kyu/find_uniq.rb
+++ lib/6kyu/find_uniq.rb
@@ -1,9 +1,10 @@
 def find_uniq(arr)
-	return arr.first if arr.length == 1
-	unique = arr.uniq
-	if arr.count(unique.first) == 1
-		unique.first
-	else
-		unique.last
-	end
+  return arr.first if arr.length == 1
+
+  unique = arr.uniq
+  if arr.count(unique.first) == 1
+    unique.first
+  else
+    unique.last
+  end
 end
--- spec/6kyu/find_uniq_spec.rb
+++ spec/6kyu/find_uniq_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../../lib/6kyu/find_uniq'
 
 RSpec.describe '#find_uniq' do
-  let(:input1) { [1,1,1,1,0] }
-	let(:input2) { [ 1, 1, 1, 2, 1, 1 ] }
-	let(:input3) { [ 0, 0, 0.55, 0, 0 ] }
+  let(:input1) { [1, 1, 1, 1, 0] }
+  let(:input2) { [1, 1, 1, 2, 1, 1] }
+  let(:input3) { [0, 0, 0.55, 0, 0] }
 
   it 'returns the odd occuring int' do
     expect(find_uniq(input1)).to eq(0)
-		expect(find_uniq(input2)).to eq(2)
-		expect(find_uniq(input3)).to eq(0.55)
+    expect(find_uniq(input2)).to eq(2)
+    expect(find_uniq(input3)).to eq(0.55)
   end
 end
