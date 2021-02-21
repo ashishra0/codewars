d883f51d885fd5da48cc26dd686ad498bb8529ca
Ashish Rao <ashishrao2598@gmail.com>
2021-02-21 22:43:24 +0530

    fix it block name

--- spec/6kyu/find_uniq_spec.rb
+++ spec/6kyu/find_uniq_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe '#find_uniq' do
   let(:input2) { [1, 1, 1, 2, 1, 1] }
   let(:input3) { [0, 0, 0.55, 0, 0] }
 
-  it 'returns the odd occuring int' do
+  it 'returns the unique number' do
     expect(find_uniq(input1)).to eq(0)
     expect(find_uniq(input2)).to eq(2)
     expect(find_uniq(input3)).to eq(0.55)
