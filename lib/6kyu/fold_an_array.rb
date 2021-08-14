162908d159556aecdd3767a41da88b0add56755e
Ashish Rao <ashishrao2598@gmail.com>
2021-08-14 21:39:13 +0530

    Add 6kyu fold an array kata and spec file

---
+++ lib/6kyu/fold_an_array.rb
@@ -0,0 +1,17 @@
+def fold_array(array, runs)
+  runs.times do
+    return [array.inject(:+)] if array.length <= 2
+
+    left_over = get_half_array(array)[1].reverse
+    array = get_half_array(array)[0]
+    diff = array.size - left_over.size
+    diff.times { left_over.push(0) }
+    array = [left_over, array].transpose.map(&:sum)
+  end
+
+  array
+end
+
+def get_half_array(array)
+  array.each_slice( (array.size/2.0).round ).to_a
+end
---
+++ spec/6kyu/fold_an_array_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../../lib/6kyu/fold_an_array'
+
+RSpec.describe '#fold_array' do
+  # Add specs
+end
