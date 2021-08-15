8b408f425e8095f249f7daee6395bccd0b051c14
Ashish Rao <ashishrao2598@gmail.com>
2021-08-15 15:57:57 +0530

    Refactor to give better name to variable

--- lib/6kyu/fold_an_array.rb
+++ lib/6kyu/fold_an_array.rb
@@ -2,11 +2,11 @@ def fold_array(array, runs)
   runs.times do
     return [array.inject(:+)] if array.length <= 2
 
-    left_over = get_half_array(array)[1].reverse
+    second_half = get_half_array(array)[1].reverse
     array = get_half_array(array)[0]
-    diff = array.size - left_over.size
-    diff.times { left_over.push(0) }
-    array = [left_over, array].transpose.map(&:sum)
+    diff = array.size - second_half.size
+    diff.times { second_half.push(0) }
+    array = [second_half, array].transpose.map(&:sum)
   end
 
   array
